3510d4bb8abad175dc2a758a87073b9198018ea3
JoseVteRS <jvrs.90@gmail.com>
2022-04-20 10:46:44 +0200

    12-183 Contador para agregar al carrito

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -4,8 +4,10 @@ import {
   CardActionArea,
   CardMedia,
   Grid,
+  Link,
   Typography,
 } from "@mui/material";
+import NextLink from "next/link";
 import { FC, useMemo, useState } from "react";
 import { IProduct } from "../../interfaces";
 
@@ -29,14 +31,18 @@ export const ProductCard: FC<Props> = ({ product }) => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            className="fadeIn"
-            image={`products/${productImage}`}
-            alt={product.title}
-          />
-        </CardActionArea>
+        <NextLink href="/product/slug" passHref prefetch={false}>
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className="fadeIn"
+                image={`products/${productImage}`}
+                alt={product.title}
+              />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
       <Box sx={{ mt: 1 }} className="fadeIn">
---
+++ pages/product/slug.tsx
@@ -0,0 +1,47 @@
+import { Box, Button, Chip, Grid, Typography } from "@mui/material";
+import { ShopLayout } from "../../components/layout";
+import { ProductSlideshow } from "../../components/products";
+import { ItemCounter } from "../../components/ui";
+import { initialData } from "../../database/products";
+
+const product = initialData.products[0];
+
+const ProductPage = () => {
+  return (
+    <ShopLayout title={product.title} pageDescription={product.description}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={7}>
+          <ProductSlideshow images={product.images} />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Box display="flex" flexDirection="column">
+            <Typography variant="h1" component="h1">
+              {product.title}
+            </Typography>
+            <Typography variant="subtitle1">${product.price}</Typography>
+
+            <Box sx={{ my: 2 }}>
+              <Typography variant="subtitle2">Cantidad</Typography>
+              {/* Item Counter */}
+              <ItemCounter />
+            </Box>
+
+            {/* Add to cart */}
+            <Button color="secondary" className="circular-btn">
+              Añadir al carrito
+            </Button>
+            <Chip label="No hay disponibles" color="error" variant="outlined" />
+
+            {/* Description */}
+            <Box sx={{ mt: 3 }}>
+              <Typography variant="subtitle2">Descipción</Typography>
+              <Typography variant="body2">{product.description}</Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default ProductPage;
